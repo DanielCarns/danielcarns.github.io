4a9635cbad90f6eecf8ad9c7a8c5a708009d5db4
Daniel Carns <carns.daniel1@gmail.com>
2023-08-14 15:27:48 -0500

    Added coming soon page(s) and sized down icons

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -2,8 +2,14 @@ import { TopNav } from "./TopNav";
 
 export default function About() {
     return (
-        <div>
+        <div className="content px-lg-5 bg-debug">
             <TopNav/>
+                <div className="mammoth-font-xxlarge">
+                    COMING SOON
+                </div>
+                <div className="">
+                    Currently working on a client site and my AWS Dev certification
+                </div>
         </div>    
     )
 }
